601c249b7d7d19f8f5319f8b0ca66c864f428657
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-03-12 08:48:02 +0800

    feat: added swagger and open api spec document viewer.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { config } from 'dotenv';
 
@@ -20,6 +21,18 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix(process.env.APP_ROOT);
   app.useGlobalPipes(new ValidationPipe());
+
+  /** Configure Swagger Module */
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Multi User Web API Project')
+    .setDescription('The Back-end Project Multi-User Systems.')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup(`${process.env.APP_ROOT}/docs`, app, document);
 
   /** Run app instance on a port */
   await app.listen(process.env.APP_PORT, () => {
