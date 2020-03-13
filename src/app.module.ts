dddf6a736725ce3ae87712246dbe9043ce43ddab
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-03-13 15:57:53 +0800

    feat: added user module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-   imports: [DatabaseModule, SharedModule],
+   imports: [DatabaseModule, SharedModule, UserModule],
    controllers: [AppController],
    providers: [],
 })
